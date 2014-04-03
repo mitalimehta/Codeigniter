839f955439d4f6950c4be432e4024ba562c4faf2
Mitali Mehta <mitali.ce@gmail.com>
2014-04-03 13:17:04 -0400

    Update user.php

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -4,7 +4,7 @@ require APPPATH.'libraries/REST_Controller.php';
 
 class User extends REST_Controller {
 
-        // example http://api2.pricenista.com/user/getuserprofile/id/X
+        
         public function getuserprofile_get(){
                 if(!($this->get('id_type'))){
                    $id_type = 1;
@@ -43,19 +43,18 @@ class User extends REST_Controller {
 
               if($user){
                
-                 //log_message('info', $user);
+                 
                  $this->response($user, 200);
               }else{
                 
-                 //log_message('error', "User not found!");
+                
                  $this->response('something went wrong', 404);
 
               }
 
         }
 
-        // example http://api2.pricenista.com/user/insertuser/user_id/xxx/ext_id/xxx/ext_type/xxx/created/xx-xx-xx xx:xx:xx/active/xxx/name/xxx/password/xxx/email/xxx@xxx.xxx/avatar/xxx.jpg/first_name/xxx/last_name/xxx/modified/xx-xx-xx xx:xx:xx/gender/{male|female}/locale/xxx/location/xxx/hometown/xxx/age/xxx
-        public function insertuser_get(){
+            public function insertuser_get(){
                 $user = array(
                            "user_id"=> '',
                            "ext_id"=> $this->get('ext_id'),
@@ -81,7 +80,7 @@ class User extends REST_Controller {
                    $this->response(array(
                                      'result:' => 'ok',
                    ));
-                   //$this->response('result:ok', 200);
+                   
                 }else{
                    log_message('error', "Error adding user!");
                    $this->response(NULL, 404);
@@ -110,83 +109,14 @@ class User extends REST_Controller {
               $userID = $this->post('user_id');
               $fb_friends = $this->post('fb_friends');
 
-              //echo "<pre>";print_r($fb_friends);
-
-              //$test = 'test string';
+              
 
               $this->response($fb_friends, 200);
-              //exit;
+             
 
         }
 
 
-
-/*
-	function allusers_get(){
-		
-		// example http://api2.pricenista.com/user/allusers/
-		
-		$this->load->model("user_db");
-		
-		$users = $this->user_db->getAllusers();
-      
-		
-        if($users)
-        {
-        	log_message('info', $users);
-            $this->response($users, 200);
-            
-        }
-
-        else
-        {
-        	log_message('error', "empty table");
-            //$this->response(NULL, 404);
-        }
-	}
-	
-	function updateuser_get()
-    {
-    	$this->load->model("user_db");
-    	
-        if(!$this->get('id'))
-        {
-        	
-        	//example http://api2.pricenista.com/user/updateuser/fb_id/mitali/fb_email/mitali%40gmail.com/fb_name/mitali/
-        	
-        	// insert record
-        	$user = array(
-        		"fb_id"=> $this->get('fb_id'),
-        	    "fb_email"=>$this->get('fb_email'),
-        	    "fb_name"=> $this->get('fb_name')
-        	);
-        	
-        	//$this->user_db->insertuser($user);
-        	
-        	$this->response($user, 400);
-        }
-        
-        else {
-        	//update
-        	
-        	//example http://api2.pricenista.com/user/updateuser/id/1/fb_id/mitali/fb_email/mitali%40gmail.com/fb_name/mitali/
-        	
-        	
-        	// insert record
-        	$user = array( 
-        		"fb_id"=> $this->get('fb_id'),
-        	    "fb_email"=>$this->get('fb_email'),
-        	    "fb_name"=> $this->get('fb_name')
-        	);
-        	
-        	$this->user_db->updateuser($user,$this->get('id'));
-        	
-        	$this->response($user, 400);
-        	}
-
-        
-        }
-*/
     }
     
   
